31b825d65e148c555354b09e75c86bbb3d70adb8
Jan Willerth <jan.willerth@seznam.cz>
2026-07-31 19:42:28 +0200

    feat(catalog): update admin for new product fields

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, VatRate
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active', 'category')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(VatRate)
+class VatRateAdmin(admin.ModelAdmin):
+    list_display = ('rate',)
